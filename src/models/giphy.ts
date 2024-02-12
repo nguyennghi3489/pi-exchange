c5db2586903243ee90d3cbc22111babcc32832d8
Nghi Nguyen <nguyennghi3489@gmail.com>
2024-02-12 08:03:40 +0700

    feat: create giphy apis (trending, search)

--- src/models/giphy.ts
+++ src/models/giphy.ts
@@ -5,11 +5,36 @@ const GiphyRating = {
   r: "Level 4",
 } as const;
 
+export interface IGiphyResponse {
+  data: IGiphy[];
+  pagination: IPagination;
+}
+
 export interface IGiphy {
   id: string;
+  images: IImages;
+  rating: keyof typeof GiphyRating;
+  slug: string;
+  title: string;
   url: string;
   username: string;
-  rating: keyof typeof GiphyRating;
-  title: string;
-  imageUrl: string;
+}
+
+export interface IImages {
+  original: IFixedHeight;
+}
+
+export interface IFixedHeight {
+  height: string;
+  size: string;
+  url: string;
+  webp: string;
+  webp_size: string;
+  width: string;
+}
+
+export interface IPagination {
+  count: number;
+  offset: number;
+  total_count: number;
 }
--- src/services/giphy.ts
+++ src/services/giphy.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { SEARCH_GIPHY_URL, TRENDING_GIPHY_URL } from "../data";
 import { IGiphy } from "../models/giphy";
 
-export const getGIFTrending = async (page: number, limit: number) => {
+export const getGIFTrending = async (page: number = 0, limit: number = 25) => {
   try {
     const response: AxiosResponse = await axios.get(
       `${TRENDING_GIPHY_URL}&offset=${page * limit}&limit=${limit}`
@@ -14,7 +14,11 @@ export const getGIFTrending = async (page: number, limit: number) => {
   }
 };
 
-export const searchGIF = async (page: number, limit: number, q: string) => {
+export const searchGIF = async (
+  q: string,
+  page: number = 0,
+  limit: number = 25
+) => {
   try {
     const response: AxiosResponse = await axios.get(
       `${SEARCH_GIPHY_URL}&q=${q}&offset=${page * limit}&limit=${limit}`
