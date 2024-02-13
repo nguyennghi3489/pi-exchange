2dece561dd3f8e428e3ab8d86aa9b26846544cfd
Nghi Nguyen <nguyennghi3489@gmail.com>
2024-02-13 22:44:17 +0700

    feat: remove unused import on giphy list item component

--- src/components/giphy-list-item/index.test.tsx
+++ src/components/giphy-list-item/index.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { GiphyListItem } from ".";
 import { IGiphy } from "../../models/giphy";
-import exp from "constants";
 import userEvent from "@testing-library/user-event";
 
 const item: IGiphy = {
